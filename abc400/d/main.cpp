bf1fe39e3e34a6a3f1c32b86c4a02e1f44fe33f5
ryu4649ryu.0114@icloud.com <sunsun@SunSundesktop.>
2025-05-17 19:59:32 +0900

    add abc400

--- abc400/d/main.cpp
+++ abc400/d/main.cpp
@@ -90,40 +90,54 @@ private:
 	// ただし root の場合は (-1 * そのグループに属する要素数)
 	std::vector<int> m_parentsOrSize;
 };
-int main(){
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    int h,w;
-	cin >> h >> w;
-	vector<string> s(h);
-	rep(i,h){
-		cin >> s[i];
-	}
+int main() {
+	int h,w;
 	int a,b,c,d;
-	cin >> a >> b >> c >> d;
+	int x,y,z,nx,ny;
+	bool wall;
+	deque<int>dq;
+	const int dx[4]={-1,1,0,0};
+	const int dy[4]={0,0,-1,1};
+	cin>>h>>w;
+	vector<string>s(h);
+	vector<vector<int> >ans(h,vector<int>(w,h*w)); 
+	for(int i=0;i<h;i++)cin>>s[i];
+	cin>>a>>b>>c>>d;
 	a--,b--,c--,d--;
-	vector<vector<int>> t(h,(w,-1));
-	queue<pair<int,int>> q;
-	vector<int> diri = {1,0,-1,0};
-	vector<int> dirj = {0,1,0,-1};
-	pari<int,int> now;
-	q.emplace(c,d);
-	while(!q.empty()){
-		now = q.front();
-		q.pop();
-		rep(i,4){
-			if(now.first + diri[i] >=0 && now.first + diri[i] < h){
-				if(now.second + dirj[i] >= 0 && now.second + dirj[i] < w){
-					if(t[now.first + diri[i]][now.second + dirj[i]] == -1){
-						q.emplace(now.first + diri[i],now.second + dirj[i]);
-						if(s[now.first + diri[i]][now.second + dirj[i]] == '.'){
-							t[now.first + diri[i]][now.second + dirj[i]] = t[now.first][now.second];
-						}else{
-
+	
+	ans[a][b]=0;
+	dq.push_front(a*w+b);
+	while(!dq.empty()){
+		z=dq.front();
+		dq.pop_front();
+		if(z==(c*w+d)){
+			cout<<ans[c][d]<<endl;
+			return 0;
+		}
+		x=z/w,y=z%w;
+		for(int i=0;i<4;i++){
+			wall=false;
+			for(int j=1;j<=2;j++){
+				nx=x+dx[i]*j;
+				ny=y+dy[i]*j;
+				if((nx<0)||(nx>=h)||(ny<0)||(ny>=w))break;
+				if(s[nx][ny]=='#')wall=true;
+				if(!wall){
+					if(j==1){
+						if(ans[nx][ny]>ans[x][y]){
+							ans[nx][ny]=ans[x][y];
+							dq.push_front(nx*w+ny);
 						}
+					}
+				}
+				else{
+					if(ans[nx][ny]>ans[x][y]+1){
+						ans[nx][ny]=ans[x][y]+1;
+						dq.push_back(nx*w+ny);
 					}
 				}
 			}
 		}
 	}
+	return 0;
 }
