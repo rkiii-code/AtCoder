bcafa7070b5b35713487d09ec836f3edad94fc61
ponchandesu <ryu4649ryu.0114@icloud.com>
2025-05-17 18:26:43 +0900

    add abc400

---
+++ abc400/b/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <set>
+#include <utility>
+#include <numeric>
+#include <algorithm>
+#include <queue>
+#include <map>
+#include <string>
+using ll = long long;
+
+using namespace std;
+using pi=pair<int,int>;
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define eb emplace_back
+
+using vi = vector<ll>;
+using vvi = vector<vi>;
+template <class T>
+using vc = vector<T>;
+template <class T>
+using vvc = vector<vc<T>>;
+class UnionFind
+{
+public:
+
+	UnionFind() = default;
+
+	/// @brief Union-Find 木を構築します。
+	/// @param n 要素数
+	explicit UnionFind(size_t n)
+		: m_parentsOrSize(n, -1) {}
+
+	/// @brief 頂点 i の root のインデックスを返します。
+	/// @param i 調べる頂点のインデックス
+	/// @return 頂点 i の root のインデックス
+	int find(int i)
+	{
+		if (m_parentsOrSize[i] < 0)
+		{
+			return i;
+		}
+
+		// 経路圧縮
+		return (m_parentsOrSize[i] = find(m_parentsOrSize[i]));
+	}
+
+	/// @brief a のグループと b のグループを統合します。
+	/// @param a 一方のインデックス
+	/// @param b 他方のインデックス
+	void merge(int a, int b)
+	{
+		a = find(a);
+		b = find(b);
+
+		if (a != b)
+		{
+			// union by size (小さいほうが子になる）
+			if (-m_parentsOrSize[a] < -m_parentsOrSize[b])
+			{
+				std::swap(a, b);
+			}
+
+			m_parentsOrSize[a] += m_parentsOrSize[b];
+			m_parentsOrSize[b] = a;
+		}
+	}
+
+	/// @brief a と b が同じグループに属すかを返します。
+	/// @param a 一方のインデックス
+	/// @param b 他方のインデックス
+	/// @return a と b が同じグループに属す場合 true, それ以外の場合は false
+	bool connected(int a, int b)
+	{
+		return (find(a) == find(b));
+	}
+
+	/// @brief i が属するグループの要素数を返します。
+	/// @param i インデックス
+	/// @return i が属するグループの要素数
+	int size(int i)
+	{
+		return -m_parentsOrSize[find(i)];
+	}
+
+private:
+
+	// m_parentsOrSize[i] は i の 親,
+	// ただし root の場合は (-1 * そのグループに属する要素数)
+	std::vector<int> m_parentsOrSize;
+};
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m;
+	cin >> n >> m;
+	ll th=1000000000;
+	ll sum = 0;
+	ll cnt = 1;
+	rep(i,m+1){
+		sum += cnt;
+		if(sum > th){
+			cout << "inf" << endl;
+			return 0;
+		}
+		cnt *= n;
+	}
+	cout << sum << endl;
+	return 0;
+}
