af3b2954e28d48d58d82302c9da97ec29c55fe40
ponchandesu <ryu4649ryu.0114@icloud.com>
2025-06-02 20:58:46 +0900

    update main README

--- abc408/d/main.cpp
+++ abc408/d/main.cpp
@@ -13,6 +13,7 @@ using ll = long long;
 using namespace std;
 using pi=pair<int,int>;
 #define rep(i,n) for(int i = 0; i < (n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
 #define eb emplace_back
 
 using vi = vector<ll>;
@@ -22,16 +23,36 @@ using vc = vector<T>;
 template <class T>
 using vvc = vector<vc<T>>;
 
+void slove(){
+	int n;
+	cin >> n;
+	std::string s;
+	cin >> s;
+	vector<int> c(n+1);
+	rep(i,n){
+		c[i+1] = c[i] + (s[i]=='0' ? 1 : -1);
+	}
+	int sum = count(s.begin(),s.end(),'1');
+	int ma=0,res=0;
+	rep(i,n+1){
+		res = min(res,c[i]-ma);
+		ma = max(ma,c[i]);
+	}
+	cout << sum + res << endl;
+}
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     int t;
 	cin >> t;
 	rep(i,t){
-		int n;
-		cin >> n;
-		unsigned s;
-		cin >> s;
-		
+		slove();
 	}
+
 }
+/*
+
+
+
+*/
+
