5bbc3d6da0bdcee9016eeedde80391f383381752
rkiii-code <ryu4649ryu.0114@icloud.com>
2025-06-08 23:58:43 +0900

    add template.cpp

--- template.cpp
+++ template.cpp
@@ -16,9 +16,10 @@ using ll = long long;
 using pi=pair<int,int>;
 void init() {cin.tie(0);ios::sync_with_stdio(false);cout << fixed << setprecision(15);}
 template<class T> using V=vector<T>;
-template<class T> using VV=vector<vector<T>>;
-template<class T> using VVV=vector<vector<vector<T>>>;
+template<class T> using VV=vector<V<T>>;
+template<class T> using VVV=vector<VV<T>>;
 const ll INF=1LL << 60;
+#define dump(x)  cout << #x << " = " << (x) << endl;
 #define press(v) v.erase( unique(v.begin(), v.end()), v.end() ) //隣接被り削除
 #define each(x,v) for(auto& x : (v))
 #define rep(i,n) for(ll i = 0; i < (n); i++)
@@ -29,6 +30,43 @@ const ll INF=1LL << 60;
 #define se second
 #define all(v) (v).begin(),(v).end()
 #define rall(v) (v).rbegin(),(v).rend()
+#define SCOUT(x) cout<<(x)<<" "
+#define ENDL cout<<endl
+#define SCOUT(x) cout<<(x)<<" "
+#define COUT(x) cout<<(x)<<endl
+void CINT(){}
+template <class Head,class... Tail>
+void CINT(Head&& head,Tail&&... tail){
+    cin>>head;
+    CINT(move(tail)...);
+}
+#define CIN(...) int __VA_ARGS__;CINT(__VA_ARGS__)
+#define LCIN(...) ll __VA_ARGS__;CINT(__VA_ARGS__)
+#define SCIN(...) string __VA_ARGS__;CINT(__VA_ARGS__)
+template <class T = ll>
+T IN(){T x;cin>>x;return (x);}
+#define VECCIN(x) for(auto&youso_: (x) )cin>>youso_
+#define VECCOUT(x) for(auto&youso_: (x) )cout<<youso_<<" ";cout<<endl
+template <class Head>
+void VT(Head head){}
+template <class Head,class Seco,class... Tail>
+void VT(Head&& head,Seco&& seco,Tail&&... tail){
+    seco.resize(head);
+    VT(head,move(tail)...);
+}
+void VT2(){}
+template <class Head,class... Tail>
+void VT2(Head&& head,Tail&&... tail){
+  VECCIN(head);
+  VT2(move(tail)...);
+}
+template <class Head>
+void VT3(Head&& head){}
+template <class Head,class Seco,class... Tail>
+void VT3(Head&& head,Seco&& seco,Tail&&... tail){
+  seco[head]=IN();
+  VT3(head,move(tail)...);
+}
 
 //
 int main(){
